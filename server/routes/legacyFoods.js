75ab8713f3fda2b1667c5379eee4d6ff4a513b45
TheFourthLion <the00110100lion@gmail.com>
2023-02-25 03:54:08 -0800

    update legacy foods to array

---
+++ server/routes/legacyFoods.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createlegacyFoods,
+  readlegacyFoods,
+  readlegacyFoodsFromID,
+  updatelegacyFoods,
+  deletelegacyFoods,
+} = require("../controllers/legacyFoods");
+router.route("/create").post(createlegacyFoods);
+router.route("/read").get(readlegacyFoods);
+router.route("/read/:id").get(readlegacyFoodsFromID);
+router.route("/update/:id").post(updatelegacyFoods);
+router.route("/delete/:id").delete(deletelegacyFoods);
+module.exports = router;
