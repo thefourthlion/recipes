3ca42f0993dae026d7dbad8693bb7f48bec5716f
TheFourthLion <the00110100lion@gmail.com>
2023-02-25 18:32:05 -0800

    build out seach ingredients

--- client/components/SearchIngredient.js
+++ client/components/SearchIngredient.js
@@ -2,32 +2,110 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import Axios from "axios";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Ingredients from "./Ingredients";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 const SearchIngredient = () => {
-  const [searchResults, setSearchResults] = useState({});
+  const [searchResults, setSearchResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const getSearchResults = Axios.get(`${searchTerm}`).then((res) => {
-    const data = res.data;
-    console.log(data);
-    setSearchResults(data);
-  });
+  const [searchId, setSearchId] = useState("");
+  const [pageSize, setPageSize] = useState("10");
+  const [pageNumber, setPageNumber] = useState("");
+  const foodUrl = `https://api.nal.usda.gov/fdc/v1/food/`;
+
+  const Ingredients = ({ name, id }) => {
+    return (
+      <div className="Ingredients">
+        <div className="container">
+          <h1 className="content-header">
+            {name}
+            <span
+              className="add-btn"
+              onClick={() => {
+                setSearchId(id);
+              }}
+            >
+              +
+            </span>
+          </h1>
+        </div>
+      </div>
+    );
+  };
+
+  const getSearchResults = () => {
+    Axios.get(
+      `https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchTerm}&pageSize=${pageSize}&api_key=W6esAlkr0u65cu999eiQXdgT7pKzhj4c9MoZbHFP`
+    ).then((res) => {
+      const data = res.data;
+      let foods = data.foods;
+      let foodNames = foods.forEach((element) =>
+        console.log(element.description)
+      );
+      // let nutrients = foods[0].foodNutrients;
+      console.log(data);
+      console.log(foods[0].fdcId);
+      console.log(foodNames);
+      // console.log(nutrients);
+      setSearchResults(data.foods);
+    });
+  };
+
+  const searchFoodItem = () => {
+    Axios.get(
+      `https://api.nal.usda.gov/fdc/v1/food/${searchId}?api_key=W6esAlkr0u65cu999eiQXdgT7pKzhj4c9MoZbHFP`
+    ).then((res) => {
+      const data = res.data;
+      console.log(data);
+    });
+  };
 
   return (
     <div className="SearchIngredient">
       <div className="container">
         <h1 className="content-header">Search Ingredient</h1>
-        <Form>
-          <FloatingLabel className="search-input-label" label="Search Books">
-            <Form.Control
-              className="search-input-form-control search-input"
-              type="text"
-              placeholder="Search Books"
-              onChange={(e) => {
-                setSearchTerm(e.target.value);
-              }}
-            ></Form.Control>
-          </FloatingLabel>
-          <button className="primary-btn">Search</button>
-        </Form>
+
+        <FloatingLabel className="search-input-label" label="Search Books">
+          <Form.Control
+            className="search-input-form-control search-input"
+            type="text"
+            placeholder="Search Books"
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          ></Form.Control>
+        </FloatingLabel>
+
+        <button
+          onClick={() => {
+            getSearchResults();
+          }}
+          className="primary-btn"
+        >
+          Search
+        </button>
+
+        <Container fluid="md">
+          <Row>
+            {searchResults.map((val) => {
+              return (
+                <Col>
+                  <Ingredients name={val.description} id={val.fdcId} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+
+        <button
+          className="primary-btn"
+          onClick={() => {
+            searchFoodItem();
+          }}
+        >
+          Search Food Item
+        </button>
       </div>
     </div>
   );
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/SearchIngredient.scss";
+import "../styles/Ingredients.scss";import "../styles/SearchIngredient.scss";
 import "../styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.css";
 function MyApp({ Component, pageProps }) {
