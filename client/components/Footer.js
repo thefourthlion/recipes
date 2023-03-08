e24a25ae203ef3a27c112e2461b3f88c260ec511
TheFourthLion <the00110100lion@gmail.com>
2023-03-07 18:30:06 -0800

    create components and add styles for navbar

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,1 +1,9 @@
-import React from "react"; const Footer = () => { return ( <div className="Footer"> <div className="container"> <h1 className="content-header">Footer</h1> </div> </div> ); }; export default Footer;
+import React from "react";
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="container"></div>
+    </div>
+  );
+};
+export default Footer;
