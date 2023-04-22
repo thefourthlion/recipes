858a5d3a1dccb75558ce591d0b8f0d08f6377e2e
TheFourthLion <the00110100lion@gmail.com>
2023-04-22 16:01:03 -0700

    make view recipe more immersive

--- client/pages/view-recipes.js
+++ client/pages/view-recipes.js
@@ -7,6 +7,10 @@ const Recipes = () => {
     getRecipe();
   }, []);
 
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+
   const getRecipe = () => {
     Axios.get("https://api.recipes.everettdeleon.com/api/recipes/read").then(
       (res) => {
@@ -17,18 +21,45 @@ const Recipes = () => {
     );
   };
 
+  const deleteRecipe = (id) => {
+    Axios.delete(
+      `https://api.recipes.everettdeleon.com/api/recipes/delete/${id}`
+    ).then((res) => {
+      refreshPage();
+    });
+  };
+
   return (
     <div className="Recipes">
       <div className="container">
         <h1 className="content-header">Recipes</h1>
         {recipes.map((val, key) => {
           return (
-            <div>
-              <h1>{val.name}</h1>
+            <div className="recipe-container">
+              <img
+                className="recipe-img"
+                src={val.image}
+                onError={(e) => {
+                  e.target.style.display = "none";
+                }}
+              />
+              <h1 className="recipe-name">{val.name}</h1>
 
               <a href={`/recipe/${val._id}`}>
                 <button className="third-btn">View Recipe</button>
               </a>
+
+              <div>
+                <br />
+                <button
+                  onClick={() => {
+                    deleteRecipe(val._id);
+                  }}
+                  className="secondary-btn"
+                >
+                  Delete Recipe
+                </button>
+              </div>
             </div>
           );
         })}
